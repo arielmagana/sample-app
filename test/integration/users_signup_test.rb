8e0c7a5eb1ab39e6ec99ac4940e1d379e53c3298
Ariel Magana <jariel88@gmail.com>
2017-09-23 01:09:15 -0500

    Add automatic log in upon sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -32,5 +32,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_select 'div.alert.alert-success', "Welcome to the Sample App!"
+    assert is_logged_in?
   end
 end
