07d0a48b1d89c2bdbdf00e6fdbfe06531294fdbc
Ariel Magana <jariel88@gmail.com>
2017-09-23 18:43:14 -0500

    Add Remember functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
@@ -13,5 +16,26 @@ class User < ApplicationRecord
     def self.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token
+    def self.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Saves the token in an attribute, as well as updating the hashed token in database
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Returns ture if the given token matches the digest
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.new name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar"
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 
 end
