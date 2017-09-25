65c28d922db7ccec8f06767626a5c80e64cda8d4
Ariel Magaña <jariel88@gmail.com>
2017-09-25 16:19:31 -0500

    Add Admin role and delete option for admins.

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,16 +2,23 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:ariel)
+    @admin = users(:ariel)
+    @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
+  test "index including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'nav.pagination', count: 2
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'Delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
     end
   end
 end
