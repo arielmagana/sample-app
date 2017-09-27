d75e051f6f9e789a209cd112ebaf91cd909c2f59
Ariel Magaña <jariel88@gmail.com>
2017-09-27 11:13:12 -0500

    Add authentication to user model and modify seeds.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
+
+    before_save :downcase_email
+    before_create :create_activation_digest
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -7,7 +10,7 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
 
-    before_save { email.downcase! }
+    # before_save { email.downcase! }
 
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
@@ -38,4 +41,16 @@ class User < ApplicationRecord
     def forget
         update_attribute(:remember_digest, nil)
     end
+
+    private
+        # Converts email to all lower-case
+        def downcase_email
+            email.downcase!
+        end
+
+        # Creaes and assigns the activation token and digest
+        def create_activation_digest
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end
