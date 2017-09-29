3fd20fb8edf54751b2db5afd63b75b6e4e9e7fd1
Ariel Magaña <jariel88@gmail.com>
2017-09-29 11:31:26 -0500

    Refine microposts and user relationship

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:ariel)
-    @micropost = Micropost.new content: "Lorem ipsum", user_id: @user.id
+    # @micropost = Micropost.new content: "Lorem ipsum", user_id: @user.id
+    @micropost = @user.microposts.build(content: "Lorem ipsum" )
   end
 
   test "should be valid" do
@@ -23,5 +24,9 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 end
