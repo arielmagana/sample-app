85b634f4c9ce2001cbf52c62a85dc23bfee73bbf
Ariel Magaña <jariel88@gmail.com>
2017-10-02 15:43:05 -0500

    Add microposts feed, delete and tests

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -6,16 +6,25 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect create when not logged in" do
-    assert_no_difference 'Microposts.count' do
+    assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
     end
     assert_redirected_to login_url
   end
 
   test "should redirect destroy when not logged in" do
-    assert_no_difference 'Microposts.count' do
+    assert_no_difference 'Micropost.count' do
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as users(:ariel)
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
   end
 end
