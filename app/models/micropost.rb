f09f9fde4cd55c9db25e4ea299d07e5b1226162b
Ariel Magaña <jariel88@gmail.com>
2017-10-04 16:38:34 -0500

    Add image resizing

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,14 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+    # Validates tge size of an uploaded Picture
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add :pitcure, "should be less than 5MB"
+      end
+    end
 end
